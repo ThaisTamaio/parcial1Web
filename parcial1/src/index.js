b5647965de63fb989005f279b94a92ebcdb9d4c1
Thais Tamaio <77994638+ThaisTamaio@users.noreply.github.com>
2023-11-01 09:43:51 -0500

    error

--- parcial1/src/index.js
+++ parcial1/src/index.js
@@ -13,6 +13,8 @@ root.render(
   </React.StrictMode>
 );
 
+error = (error) => {
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
