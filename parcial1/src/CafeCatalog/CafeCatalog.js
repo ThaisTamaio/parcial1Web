c34d386b0a0454a2ef49831832dc87dfcc4ee166
Thais Tamaio <77994638+ThaisTamaio@users.noreply.github.com>
2023-11-01 09:51:52 -0500

    Update CafeCatalog.js

--- parcial1/src/CafeCatalog/CafeCatalog.js
+++ parcial1/src/CafeCatalog/CafeCatalog.js
@@ -5,6 +5,7 @@ import './CafeCatalog.css';
 
 function CafeCatalog({ cafes, onSelectCafe, selectedCafe, fieldValues }) {
     return (
+        error{}
         <Col md={6}>
             <Table hover className="cafe-table">
                 <thead>
