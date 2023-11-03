3d554580057b6a8b84bb575b627a40185e8c7da1
Thais Tamaio <77994638+ThaisTamaio@users.noreply.github.com>
2023-11-02 20:12:31 -0500

    Fixing the bug

--- src/CafeDetails/CafeDetails.js
+++ src/CafeDetails/CafeDetails.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState, useCallback } from 'react';
 import { Col, Card } from 'react-bootstrap';
 import { FormattedMessage } from "react-intl";
-import { FormattedDate } from 'react-intl';
+import {FormattedDate} from 'react-intl';
 import './CafeDetails.css';
 
 function CafeDetails({ selectedCafe }) {
@@ -10,7 +10,7 @@ function CafeDetails({ selectedCafe }) {
 
   const fetchCafeDetails = useCallback(async () => {
     if (selectedCafe && selectedCafe.id) {
-      const response = await fetch(`http://localhost:3001/caefes/${selectedCafe.id}`);
+      const response = await fetch(`http://localhost:3001/cafes/${selectedCafe.id}`);
       if (!response.ok) {
         throw new Error("Error al obtener los detalles del café");
       }
@@ -29,7 +29,7 @@ function CafeDetails({ selectedCafe }) {
         .then(response => response.blob())
         .then(blob => {
           const url = URL.createObjectURL(blob);
-          setImageSrc(imageScr);
+          setImageSrc(url);
         })
         .catch(error => console.error('Error fetching image:', error));
     }
@@ -37,7 +37,7 @@ function CafeDetails({ selectedCafe }) {
 
   return (
     <Col md={4}>
-      <div className="cafe-details">
+      <div className="cafe-details"> 
         <Card className="cafe-card">
           <Card.Body>
             <Card.Text className="cafe-name">
